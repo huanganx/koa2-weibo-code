fa31b54a12b29657a436f2f7a573c8ba96358468
huanganx <1039215157@qq.com>
2020-10-11 01:00:16 +0800

    feat:ejs 演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,45 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: true,
+    blogList: [{
+        id: 1,
+        title: 'aaa'
+      },
+      {
+        id: 2,
+        title: 'bbb'
+      }
+    ]
   })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:username', async (ctx, next) => {
+  const {
+    username
+  } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    username
+  }
+})
+
+router.get('/loadMore/:username/:pageIndex', async (ctx, next) => {
+  const {
+    username,
+    pageIndex
+  } = ctx.params
+  ctx.body = {
+    title: 'this is loadMore page',
+    username,
+    pageIndex
   }
 })
 
