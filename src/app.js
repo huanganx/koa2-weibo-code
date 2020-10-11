c0ed46b28fb6177e4842edd53a8245a0089e4576
huanganx <1039215157@qq.com>
2020-10-11 14:36:34 +0800

    feat: 增加 session 配置

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+const {
+  REDIS_CONF
+} = require('./conf/db')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -14,7 +19,7 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
@@ -22,6 +27,21 @@ app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
+}))
+
+// session 配置
+app.keys = ['GFd$h2H_wR#']
+app.use(session({
+  key: 'weibo.sid', // cookie name
+  prefix: 'weibo:sess:', // redis key 前缀
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
 }))
 
 // logger
